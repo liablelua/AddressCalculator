f3b76786ff7b6ed752bc2318865d147675e9ba1a
Lua <140775902+liablelua@users.noreply.github.com>
2024-09-23 09:13:34 -0500

    Address Calculator

---
+++ AddressCalculator.cpp
@@ -0,0 +1,11 @@
+#include "AddressCalculator.h"
+
+long long subtract(unsigned long long address1, unsigned long long address2) {
+    long long result = address1 - address2;
+    return result;
+}
+
+long long add(unsigned long long address1, unsigned long long address2) {
+    long long result = address1 + address2;
+    return result;
+}
