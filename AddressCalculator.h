60abdcd37e707e16577290f3a38dec73d495c5e6
Lua <140775902+liablelua@users.noreply.github.com>
2024-09-23 09:25:28 -0500

    Create AddressCalculator.h

---
+++ AddressCalculator.h
@@ -0,0 +1,8 @@
+#include <iostring>
+#include <iomanip>
+
+class ac {
+public:
+  long long subtract(unsigned long long address1, unsigned long long address2);
+  long long add(unsigned long long address1, unsigned long long address2);
+};
